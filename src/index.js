5494b07cec7019a3fdb25692de7ac1b394911f57
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-08-18 14:20:08 +0300

    feat: add proper method functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    const sortedArray = [];
+    if (matrix === undefined) return [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i === 0 || i % 2 === 0) {
+            sortedArray.push(...matrix[i]);
+        } else {
+            sortedArray.push(...matrix[i].reverse());
+        }
+    }
+    return sortedArray;
+};
